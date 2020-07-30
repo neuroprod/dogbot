da1a72407dca6fee448ff4032ae4497807791556
Kris Temmerman <kris@neuroproductions.be>
2020-07-30 11:44:54 +0200

    usbrules

--- code/src/motor/MotorControl.cpp
+++ code/src/motor/MotorControl.cpp
@@ -8,12 +8,15 @@ using namespace std;
 //115200
 void MotorControl::setup() 
 {
-
+	//udevadm info --name=/dev/ttyUSBx --attribute-walk
+	///set device in: etc/udev/rules.d/99-usb-serial.rules
+	// SUBSYSTEM=="tty", ATTRS{idVendor}=="1234", ATTRS{idProduct}=="5678", SYMLINK+="your_device_name"
+	//reload: sudo udevadm trigger
     for( const auto &dev : Serial::getDevices() )
         console() << "Device: " << dev.getName() << endl;
 
 	MotorRef FRHip = Motor::create();
-	FRHip->setup(1, "FRHip","ttyUSB0");
+	FRHip->setup(1, "FRHip","ttyMotor1");
 	motors.push_back(FRHip);	
 }
 
